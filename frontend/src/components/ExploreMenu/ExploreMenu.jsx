5867410e6e0eb095e8d3285a7e00efbd35e402dc
GAP-Pathum <pathumpasindu41@gmail.com>
2025-01-04 09:02:16 +0530

    Added all changes to the home branch

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
 
-function ExploreMenu() {
+const ExploreMenu = ({category, setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore Our Menu</h1>
@@ -10,13 +10,14 @@ function ExploreMenu() {
       <div className='explore-menu-list'>
         {menu_list.map((item, index) => {
           return (
-            <div className='explore-menu-list-item' key={index}>
+            <div onClick={()=>setCategory(prev=>prev==item.menu_image.name?"All":item.menu_name)} className='explore-menu-list-item' key={index}>
               <img src={item.menu_image} alt={item.name} />
               <p>{item.menu_name}</p>
             </div>
           )
         })}
       </div>
+      <hr />
     </div>
   )
 }
