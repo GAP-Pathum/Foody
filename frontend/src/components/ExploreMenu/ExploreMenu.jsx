caba367315e4e0b2565f80716670cc585af3bd1a
GAP-Pathum <pathumpasindu41@gmail.com>
2025-01-12 23:43:25 +0530

    Add Footer component, enhance FoodItem functionality, and implement cart management in StoreContext

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -10,8 +10,8 @@ const ExploreMenu = ({category, setCategory}) => {
       <div className='explore-menu-list'>
         {menu_list.map((item, index) => {
           return (
-            <div onClick={()=>setCategory(prev=>prev===item.menu_image.name?"All":item.menu_name)} className='explore-menu-list-item' key={index}>
-              <img className={category===item.menu_name?"active":" "} src={item.menu_image} alt={item.name} />
+            <div onClick={() => setCategory(prev => prev === item.menu_name ? "All" : item.menu_name)} className='explore-menu-list-item' key={index}>
+              <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt={item.name} />
               <p>{item.menu_name}</p>
             </div>
           )
--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-import './FoodItem.csss'
+import React, { useContext } from 'react'
+import './FoodItem.css'
+import {assets} from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
 
-const FoodItem = () => {
+const FoodItem = ({id,name,price,description,image}) => {
+
+  const {cartItems, addToCart, removeFromCart} = useContext(StoreContext);
+
+
   return (
-    <div>
+    <div className='food-item'>
+      <div className='food-item-img-container'>
+        <img src={image} alt={name} className='food-item-img'/>
+        {!cartItems[id]
+         ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt='add'/>
+          :<div className='food-item-counter'>
+          <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt='remove'/>
+          <p>{cartItems[id]}</p>
+          <img onClick={()=>addToCart(id)}  src={assets.add_icon_green} alt='add'/>
+          </div>
+        }
+      </div>
+      <div className='food-item-info'>
+        <div className='food-item-name-rating'><p>{name}</p>
+        <img src={assets.rating_starts}/>
+        </div>
+        <p className='food-item-desc'>{description}</p>
+        <p className='food-item-price'>${price}</p>
+      </div>
       
     </div>
   )
