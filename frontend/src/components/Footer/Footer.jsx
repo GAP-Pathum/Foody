8b6ff41461eda572d68bdf6d5b94b28a153bc3f2
GAP-Pathum <pathumpasindu41@gmail.com>
2025-01-14 12:55:31 +0530

    Add AppDownload component and enhance footer layout; update ExploreMenu styles for responsiveness

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
     <div className="footer" id="footer">
       <div className="footer-content">
         <div className='footer-content-left'>
-          <img src={assets.logo} alt='logo' />
+          <img src={assets.logo} className='logo' alt='logo' />
           <p>Foody is your go-to food delivery app, bringing delicious meals from your favorite 
           restaurants right to your doorstep. Enjoy a wide variety of cuisines and fast, reliable service.</p>
           <div className="footer-social-icons">
@@ -16,13 +16,25 @@ function Footer() {
             <img src={assets.linkedin_icon} alt="" />
           </div>
         </div>
-        <div className='footer-content-right'>
-
-        </div>
         <div className='footer-content-center'>
-
+          <h2>Company</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy & Policy</li>
+          </ul>
+        </div>
+        <div className='footer-content-right'>
+          <h2>Get in touch</h2>
+          <ul>
+            <li>075 29 55 301</li>
+            <li>foody@info.com</li>
+          </ul>
         </div>
       </div>
+      <hr />
+      <p className='footer-copyright'>Copyright 2025 @ GAP-Pathum. All Rights Reserved.</p>
     </div>
   )
 }
